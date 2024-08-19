b37bbb133e8afdb5426a49b5394a0205271ebeee
raphsenn <raphaelsenn@outlook.de>
2024-08-19 18:50:52 +0200

    update test

--- tests/test_logistic_regression.py
+++ tests/test_logistic_regression.py
@@ -7,9 +7,9 @@ import numpy as np
 def test_logistic_regression_1():
     X = np.array([[0], [1]])
     y = np.array([1, 0])
-    or_gate= LogisticRegression(1)
-    or_gate.train(X, y, epochs=1000, learning_rate=0.1)
-    assert np.array_equal(or_gate.predict(X), y)
+    nor = LogisticRegression(1)
+    nor.train(X, y, epochs=1000, learning_rate=0.1)
+    assert np.array_equal(nor.predict(X), y)
 
 
 def test_logistic_regression_2():
@@ -23,6 +23,6 @@ def test_logistic_regression_2():
 def test_logistic_regression_3():
     X = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
     y = np.array([0, 0, 0, 1])
-    or_gate= LogisticRegression(2)
-    or_gate.train(X, y, epochs=1000, learning_rate=0.1)
-    assert np.array_equal(or_gate.predict(X), y)
+    and_gate= LogisticRegression(2)
+    and_gate.train(X, y, epochs=1000, learning_rate=0.1)
+    assert np.array_equal(and_gate.predict(X), y)
